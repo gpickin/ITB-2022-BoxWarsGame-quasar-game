9097c5c2673ea3c5e7fc5cfd95b413d28007e14f
Gavin Pickin <gpickin@gmail.com>
2022-09-13 16:48:22 -0700

    Update Index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,11 +2,28 @@
 	<div class="q-pa-sm">
 		<div class="text-center">
 			<div class="text-center q-px-md">
-				<label class="text-h4">Enter Your Name</label>
+				<label class="text-h4">Ready to play?</label>
 				<q-input
 					v-model="name"
+					label="Name"
+					stack-label
+					placeholder="Enter your Name"
 					v-on:keyup.enter="startGame()"
 					class="text-h5"
+					:error="$v.name.$invalid"
+					error-message="Please enter a Valid Name"
+					:bottom-slots="true"
+				/>
+				<q-input
+					label="Email Address"
+					stack-label
+					placeholder="Enter your Email Address"
+					v-model="email"
+					v-on:keyup.enter="startGame()"
+					class="text-h5"
+					:error="$v.email.$invalid"
+					error-message="Please enter a Valid Email"
+					:bottom-slots="true"
 				/>
 			</div>
 
@@ -25,18 +42,45 @@
 </template>
 
 <script>
+	import {
+		required,
+		minLength,
+		email,
+		maxLength,
+	} from "vuelidate/lib/validators";
+
 	export default {
 		name: "PageIndex",
 		data() {
 			return {
 				name: "",
+				email: "",
 			};
+		},
+		validations: {
+			name: {
+				required,
+				minLength: minLength(3),
+				maxLength: maxLength(255),
+			},
+			email: {
+				required,
+				email,
+				minLength: minLength(1),
+				maxLength: maxLength(255),
+			},
+		},
+		mounted() {
+			this.$nextTick(() => {
+				this.name = this.$route.query.name;
+				this.email = this.$route.query.email;
+			});
 		},
 		methods: {
 			startGame: function () {
 				this.$router.push({
 					path: "/game",
-					query: { name: this.name },
+					query: { name: this.name, email: this.email },
 				});
 			},
 		},
